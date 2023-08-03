2824019836180b407aca40c1fdc1a3f85ac8ecd8
SenpaiKirigaia <pavlushkin03@mail.ru>
2023-08-03 15:24:01 +0300

    Project added to git

---
+++ README.md
@@ -0,0 +1,45 @@
+# Quantum GAN with Hybrid Generator
+PennyLane and Pytorch implementation of QGAN-HG: Quantum generative models for small molecule drug discovery, based on MolGAN (https://arxiv.org/abs/1805.11973)  
+This library refers to the following source code.
+* [jundeli/quantum-gan](https://github.com/jundeli/quantum-gan)
+* [yongqyu/MolGAN-pytorch](https://github.com/yongqyu/MolGAN-pytorch)
+* [nicola-decao/MolGAN](https://github.com/nicola-decao/MolGAN)
+* [yunjey/StarGAN](https://github.com/yunjey/StarGAN)
+
+
+For details see [Quantum Generative Models for Small Molecule Drug Discovery](https://arxiv.org/abs/2101.03438) by Junde Li, Rasit Topaloglu, and Swaroop Ghosh.
+
+
+## Dependencies
+
+* **python>=3.5**
+* **pytorch>=0.4.1**: https://pytorch.org
+* **rdkit**: https://www.rdkit.org
+* **pennylane**
+* **tensorflow==1.15**
+* **frechetdist**
+
+## Structure
+* [data](https://github.com/jundeli/quantum-gan/data): should contain your datasets. If you run `download_dataset.sh` the script will download the dataset used for the paper (then you should run `data/sparse_molecular_dataset.py` to conver the dataset in a graph format used by MolGAN models).
+* [models](https://github.com/jundeli/quantum-gan/models.py): Class for Models.
+
+## Training
+```
+python main.py --quantum True --circuit main --layer 2 --qubits 10 --complexity hr
+```
+If you want to run classical MolGAN, please set quantum argument to False. But you can still train reduced models by setting complexity to 'hr'-highly reduced (around 2% of original generator papameters), 'mr'-moderately reduced (around 15%), or 'nr'-no reduce. Layer and qubits can adjust expressive power of variational quantum circuit.
+
+
+## Demo
+You can see generated small molecules with pretrined models which are included in qgan-hg/models. Quantum circuit parameters are shown in gen_weights.csv. Inference can be done on either PennyLane quantum simulator or real IBM quantum computers.
+
+```
+comparison-demo.ipynb
+```
+Below are some generated molecules:
+
+<div style="color:#0000FF" align="center">
+<img src="molecules/mol1.png" width="430"/> 
+<img src="molecules/mol2.png" width="430"/>
+</div>
+
---
+++ solver.py
@@ -0,0 +1,223 @@
+import numpy as np
+import os
+import time
+import datetime
+
+import torch
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torchvision.utils import save_image
+import torch.utils.data as data
+
+from utils import *
+from models import Generator, Discriminator
+from data.sparse_molecular_dataset import SparseMolecularDataset
+
+from frechetdist import frdist
+import csv
+
+
+class Solver(object):
+    """Solver for training and testing StarGAN."""
+
+    def __init__(self, config):
+        """Initialize configurations."""
+
+        # Data loader.
+        self.data = SparseMolecularDataset()
+        self.data.load(config.mol_data_dir)
+
+        # Model configurations.
+        self.z_dim = config.qubits
+        self.m_dim = self.data.atom_num_types
+        self.b_dim = self.data.bond_num_types
+        self.g_conv_dim = config.g_conv_dim
+        self.d_conv_dim = config.d_conv_dim
+        self.g_repeat_num = config.g_repeat_num
+        self.d_repeat_num = config.d_repeat_num
+        self.lambda_cls = config.lambda_cls
+        self.lambda_rec = config.lambda_rec
+        self.lambda_gp = config.lambda_gp
+        self.post_method = config.post_method
+
+        self.metric = 'validity,sas'
+
+        # Training configurations.
+        self.batch_size = config.batch_size
+        self.num_iters = config.num_iters
+        self.num_iters_decay = config.num_iters_decay
+        self.g_lr = config.g_lr
+        self.d_lr = config.d_lr
+        self.dropout = config.dropout
+        self.n_critic = config.n_critic
+        self.beta1 = config.beta1
+        self.beta2 = config.beta2
+        self.resume_iters = config.resume_iters
+
+        # Test configurations.
+        self.test_iters = config.test_iters
+
+        # Miscellaneous.
+        self.use_tensorboard = config.use_tensorboard
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        # Directories.
+        self.log_dir = config.log_dir
+        self.sample_dir = config.sample_dir
+        self.model_save_dir = config.model_save_dir
+        self.result_dir = config.result_dir
+
+        # Step size.
+        self.log_step = config.log_step
+        self.sample_step = config.sample_step
+        self.model_save_step = config.model_save_step
+        self.lr_update_step = config.lr_update_step
+
+        # Build the model and tensorboard.
+        self.build_model()
+        if self.use_tensorboard:
+            self.build_tensorboard()
+
+        self.quantum = config.quantum
+        self.layer = config.layer
+        self.qubits = config.qubits
+
+    def build_model(self):
+        """Create a generator and a discriminator."""
+        self.G = Generator(self.g_conv_dim, self.z_dim,
+                           self.data.vertexes,
+                           self.data.bond_num_types,
+                           self.data.atom_num_types,
+                           self.dropout)
+        self.D = Discriminator(self.d_conv_dim, self.m_dim, self.b_dim, self.dropout)
+        self.V = Discriminator(self.d_conv_dim, self.m_dim, self.b_dim, self.dropout)
+
+        self.g_optimizer = torch.optim.Adam(list(self.G.parameters()) + list(self.V.parameters()),
+                                            self.g_lr, [self.beta1, self.beta2])
+        self.d_optimizer = torch.optim.Adam(self.D.parameters(), self.d_lr, [self.beta1, self.beta2])
+        self.print_network(self.G, 'G')
+        self.print_network(self.D, 'D')
+
+        self.G.to(self.device)
+        self.D.to(self.device)
+        self.V.to(self.device)
+
+    def print_network(self, model, name):
+        """Print out the network information."""
+        num_params = 0
+        for p in model.parameters():
+            num_params += p.numel()
+        print(model)
+        print(name)
+        print("The number of parameters: {}".format(num_params))
+
+    def restore_model(self, resume_iters):
+        """Restore the trained generator and discriminator."""
+        print('Loading the trained models from step {}...'.format(resume_iters))
+        G_path = os.path.join(self.model_save_dir, '{}-G.ckpt'.format(resume_iters))
+        D_path = os.path.join(self.model_save_dir, '{}-D.ckpt'.format(resume_iters))
+        V_path = os.path.join(self.model_save_dir, '{}-V.ckpt'.format(resume_iters))
+        self.G.load_state_dict(torch.load(G_path, map_location=lambda storage, loc: storage))
+        self.D.load_state_dict(torch.load(D_path, map_location=lambda storage, loc: storage))
+        self.V.load_state_dict(torch.load(V_path, map_location=lambda storage, loc: storage))
+
+    def build_tensorboard(self):
+        """Build a tensorboard logger."""
+        from logger import Logger  # from logger import Logger
+        self.logger = Logger(self.log_dir)
+
+    def update_lr(self, g_lr, d_lr):
+        """Decay learning rates of the generator and discriminator."""
+        for param_group in self.g_optimizer.param_groups:
+            param_group['lr'] = g_lr
+        for param_group in self.d_optimizer.param_groups:
+            param_group['lr'] = d_lr
+
+    def reset_grad(self):
+        """Reset the gradient buffers."""
+        self.g_optimizer.zero_grad()
+        self.d_optimizer.zero_grad()
+
+    def denorm(self, x):
+        """Convert the range from [-1, 1] to [0, 1]."""
+        out = (x + 1) / 2
+        return out.clamp_(0, 1)
+
+    def gradient_penalty(self, y, x):
+        """Compute gradient penalty: (L2_norm(dy/dx) - 1)**2."""
+        weight = torch.ones(y.size()).to(self.device)
+        dydx = torch.autograd.grad(outputs=y,
+                                   inputs=x,
+                                   grad_outputs=weight,
+                                   retain_graph=True,
+                                   create_graph=True,
+                                   only_inputs=True)[0]
+
+        dydx = dydx.view(dydx.size(0), -1)
+        dydx_l2norm = torch.sqrt(torch.sum(dydx ** 2, dim=1))
+        return torch.mean((dydx_l2norm - 1) ** 2)
+
+    def label2onehot(self, labels, dim):
+        """Convert label indices to one-hot vectors."""
+        out = torch.zeros(list(labels.size()) + [dim]).to(self.device)
+        out.scatter_(len(out.size()) - 1, labels.unsqueeze(-1), 1.)
+        return out
+
+    def classification_loss(self, logit, target, dataset='CelebA'):
+        """Compute binary or softmax cross entropy loss."""
+        if dataset == 'CelebA':
+            return F.binary_cross_entropy_with_logits(logit, target, size_average=False) / logit.size(0)
+        elif dataset == 'RaFD':
+            return F.cross_entropy(logit, target)
+
+    def sample_z(self, batch_size):
+        return np.random.normal(0, 1, size=(batch_size, self.z_dim))
+
+    def postprocess(self, inputs, method, temperature=1.):
+
+        def listify(x):
+            return x if type(x) == list or type(x) == tuple else [x]
+
+        def delistify(x):
+            return x if len(x) > 1 else x[0]
+
+        if method == 'soft_gumbel':
+            softmax = [F.gumbel_softmax(e_logits.contiguous().view(-1, e_logits.size(-1))
+                                        / temperature, hard=False).view(e_logits.size())
+                       for e_logits in listify(inputs)]
+        elif method == 'hard_gumbel':
+            softmax = [F.gumbel_softmax(e_logits.contiguous().view(-1, e_logits.size(-1))
+                                        / temperature, hard=True).view(e_logits.size())
+                       for e_logits in listify(inputs)]
+        else:
+            softmax = [F.softmax(e_logits / temperature, -1)
+                       for e_logits in listify(inputs)]
+
+        return [delistify(e) for e in (softmax)]
+
+    def reward(self, mols):
+        rr = 1.
+        for m in ('logp,sas,qed,unique' if self.metric == 'all' else self.metric).split(','):
+
+            if m == 'np':
+                rr *= MolecularMetrics.natural_product_scores(mols, norm=True)
+            elif m == 'logp':
+                rr *= MolecularMetrics.water_octanol_partition_coefficient_scores(mols, norm=True)
+            elif m == 'sas':
+                rr *= MolecularMetrics.synthetic_accessibility_score_scores(mols, norm=True)
+            elif m == 'qed':
+                rr *= MolecularMetrics.quantitative_estimation_druglikeness_scores(mols, norm=True)
+            elif m == 'novelty':
+                rr *= MolecularMetrics.novel_scores(mols, data)
+            elif m == 'dc':
+                rr *= MolecularMetrics.drugcandidate_scores(mols, data)
+            elif m == 'unique':
+                rr *= MolecularMetrics.unique_scores(mols)
+            elif m == 'diversity':
+                rr *= MolecularMetrics.diversity_scores(mols, data)
+            elif m == 'validity':
+                rr *= MolecularMetrics.valid_scores(mols)
+            else:
+                raise RuntimeError('{} is not defined as a metric'.format(m))
+
+        return rr.reshape(-1, 1)
